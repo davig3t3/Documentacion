0b40a684058221f888658b4eaf57f1e63548f138
davig3t3 <davig3t3@gmail.com>
2022-10-13 20:51:19 -0500

    Proyecto Java (clase)
    Se sube todo el core del proyecto de clase

---
+++ clase/Budget/Budget_Domain/src/edu/uco/budget/domain/PersonDTO.java
@@ -0,0 +1,90 @@
+package edu.uco.budget.domain;
+
+import static edu.uco.budget.crosscutting.helper.StringHelper.EMPTY;
+import static edu.uco.budget.crosscutting.helper.StringHelper.applyTrim;
+import static edu.uco.budget.crosscutting.helper.UUIDHelper.getDefaultUUID;
+import static edu.uco.budget.crosscutting.helper.UUIDHelper.getNewUUID;
+
+import java.util.UUID;
+
+public final class PersonDTO {
+
+	private UUID id;
+	private String idCard;
+	private String firstName;
+	private String secondName;
+	private String firstSurname;
+	private String secondSurname;
+
+	public PersonDTO() {
+		setId(getNewUUID());
+		setIdCard(EMPTY);
+		setFirstName(EMPTY);
+		setSecondName(EMPTY);
+		setFirstSurname(EMPTY);
+		setSecondSurname(EMPTY);
+	}
+
+	public PersonDTO(final UUID id, final String idCard, final String firstName, String secondName, String firstSurname,
+			String secondSurname) {
+		setId(id);
+		setIdCard(idCard);
+		setFirstName(firstName);
+		setSecondName(secondName);
+		setFirstSurname(firstSurname);
+		setSecondSurname(secondSurname);
+	}
+
+	public static final PersonDTO create(final UUID id, final String idCard, final String firstName, String secondName,
+			String firstSurname, String secondSurname) {
+		return new PersonDTO(id, idCard, firstName, secondName, firstSurname, secondSurname);
+	}
+
+	public final UUID getId() {
+		return id;
+	}
+
+	public final void setId(final UUID id) {
+		this.id = getDefaultUUID(id);
+	}
+
+	public final String getIdCard() {
+		return idCard;
+	}
+
+	public final void setIdCard(final String idCard) {
+		this.idCard = applyTrim(idCard);
+	}
+
+	public final String getFirstName() {
+		return firstName;
+	}
+
+	public final void setFirstName(final String firstName) {
+		this.firstName = applyTrim(firstName);
+	}
+
+	public final String getSecondName() {
+		return secondName;
+	}
+
+	public final void setSecondName(final String secondName) {
+		this.secondName = applyTrim(secondName);
+	}
+
+	public final String getFirstSurname() {
+		return firstSurname;
+	}
+
+	public final void setFirstSurname(final String firstSurname) {
+		this.firstSurname = applyTrim(firstSurname);
+	}
+
+	public final String getSecondSurname() {
+		return secondSurname;
+	}
+
+	public final void setSecondSurname(final String secondSurname) {
+		this.secondSurname = applyTrim(secondSurname);
+	}
+}
---
+++ clase/Budget/Budget_Domain/src/edu/uco/budget/domain/YearDTO.java
@@ -0,0 +1,44 @@
+package edu.uco.budget.domain;
+
+import static edu.uco.budget.crosscutting.helper.NumberHelper.ZERO;
+import static edu.uco.budget.crosscutting.helper.NumberHelper.isLessThan;
+import static edu.uco.budget.crosscutting.helper.UUIDHelper.getDefaultUUID;
+import static edu.uco.budget.crosscutting.helper.UUIDHelper.getNewUUID;
+
+import java.util.UUID;
+
+public final class YearDTO {
+
+	private UUID id;
+	private short yearNumber;
+
+	public YearDTO() {
+		setId(getNewUUID());
+		setYearNumber(ZERO);
+	}
+
+	public YearDTO(final UUID id, final short yearNumber) {
+		setId(id);
+		setYearNumber(yearNumber);
+	}
+
+	public static final YearDTO create(final UUID id, final short yearNumber) {
+		return new YearDTO(id, yearNumber);
+	}
+
+	public final UUID getId() {
+		return id;
+	}
+
+	public final void setId(final UUID id) {
+		this.id = getDefaultUUID(id);
+	}
+
+	public final short getYearNumber() {
+		return yearNumber;
+	}
+
+	public final void setYearNumber(final short yearNumber) {
+		this.yearNumber = isLessThan(yearNumber, ZERO) ? ZERO : yearNumber;
+	}
+}
