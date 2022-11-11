bd6cbe0018ccb9330dbd657ab47aea5deae72fc2
Miguel <miguelriostangarife@gmail.com>
2022-11-10 22:14:34 -0500

    Update DTO

--- extraclase/Apipet/Apipet_Domain/src/pet/apipet/domain/builder/BasicCareDTOBuilder.java
+++ extraclase/Apipet/Apipet_Domain/src/pet/apipet/domain/builder/BasicCareDTOBuilder.java
@@ -1,5 +1,42 @@
 package pet.apipet.domain.builder;
 
-public class BasicCareDTOBuilder {
+import java.util.UUID;
 
+import pet.apipet.domain.BasicCareDTO;
+
+public class BasicCareDTOBuilder {
+	
+	private UUID id_care;
+	private String name;
+	private short Date;
+	
+	private BasicCareDTOBuilder() {
+		super();
+	}
+	
+	public static final BasicCareDTOBuilder getBasicCareDTOBuilder(){
+		return new BasicCareDTOBuilder();
+	}
+
+	public final BasicCareDTOBuilder setId_care(UUID id_care) {
+		this.id_care = id_care;
+		return this;
+	}
+
+	public final BasicCareDTOBuilder setName(String name) {
+		this.name = name;
+		return this;
+	}
+
+	public final BasicCareDTOBuilder setDate(short date) {
+		Date = date;
+		return this;
+	}
+	
+	public final BasicCareDTO build() {
+		return BasicCareDTO.create(id_care, name, Date);
+	}
+	
+	
+	
 }
--- extraclase/Apipet/Apipet_Domain/src/pet/apipet/domain/builder/CategoryLocationDTOBuilder.java
+++ extraclase/Apipet/Apipet_Domain/src/pet/apipet/domain/builder/CategoryLocationDTOBuilder.java
@@ -1,5 +1,34 @@
 package pet.apipet.domain.builder;
 
+import java.util.UUID;
+import pet.apipet.domain.CategoryLocationDTO;
+
 public class CategoryLocationDTOBuilder {
+	
+	private UUID id_categoryLocation; 
+	private String category;
+	
+	private CategoryLocationDTOBuilder() {
+		super();
+	}
+	
+	public static final CategoryLocationDTOBuilder getCategoryLocationDTOBuilder(){
+		return new CategoryLocationDTOBuilder();
+	}
+
+	public final CategoryLocationDTOBuilder setId_categoryLocation(UUID id_categoryLocation) {
+		this.id_categoryLocation = id_categoryLocation;
+		return this;
+	}
+
+	public final CategoryLocationDTOBuilder setCategory(String category) {
+		this.category = category;
+		return this;
+	}
+	
+	public final CategoryLocationDTO build() {
+		return CategoryLocationDTO.create(id_categoryLocation, category);
+	}
+	
 
 }
