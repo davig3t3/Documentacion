5778a719f532d31523c0055a05d5a8dee73c2de6
Miguel <miguelriostangarife@gmail.com>
2022-11-11 20:33:38 -0500

    Update clase 11112022

---
+++ clase/Budget/Budget_Crosscutting/src/edu/uco/budget/crosscutting/messages/Message.java
@@ -0,0 +1,60 @@
+package edu.uco.budget.crosscutting.messages;
+
+import edu.uco.budget.crosscutting.helper.ObjectHelper;
+import edu.uco.budget.crosscutting.helper.StringHelper;
+import edu.uco.budget.crosscutting.messages.enumeration.MessageLevel;
+
+public final class Message {
+	private MessageLevel level;
+	private String content;
+	
+	public Message() {
+		setLevel(MessageLevel.FATAL);
+		setContent(StringHelper.EMPTY);
+	}
+	
+	public Message(MessageLevel level, String content) {
+		super();
+		setLevel(level);
+		setContent(content);
+	}
+	
+	public static Message createFatalMessage(final String content) {
+		return new Message(MessageLevel.FATAL,content);
+	}
+	
+	public static Message createErrorMessage(final String content) {
+		return new Message(MessageLevel.ERROR,content);
+	}
+	
+	public static Message createWarningMessage(final String content) {
+		return new Message(MessageLevel.WARNING,content);
+	}
+	
+	public static Message createInfoMessage(final String content) {
+		return new Message(MessageLevel.INFO,content);
+	}
+	
+	public static Message createSuccesMessage(final String content) {
+		return new Message(MessageLevel.SUCCESS,content);
+	}
+
+	public MessageLevel getLevel() {
+		return level;
+	}
+
+	public void setLevel(MessageLevel level) {
+		this.level = ObjectHelper.getDefaultIfNull(level, MessageLevel.FATAL);
+	}
+	
+	public String getContent() {
+		return content;
+	}
+
+	public final void setContent(String content) {
+		this.content = StringHelper.applyTrim(content);
+	}
+
+	
+	
+}
